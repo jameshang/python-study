1da71b41a71120c8660ee59da016752ce1bf55da
Architect2949 <architect2949@gmail.com>
2020-03-29 16:21:41 +0800

    update file
    1. hello-world.py

--- hello-world/com/hh/hw/hello-world.py
+++ hello-world/com/hh/hw/hello-world.py
@@ -1,6 +1,32 @@
-def hello(name):
-    print('Hello %s !' % name)
+from tkinter import *
+
+
+def hello1(name):
+    print('Hello %s!' % name)
+
+
+def hello2(name):
+    root = Tk()
+    lb = Label(root, text='Hello %s!' % name)
+    lb.pack()
+    root.mainloop()
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def plus(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
 
 
 if __name__ == '__main__':
-    hello('Hanson')
+    hello2('Hanson')
