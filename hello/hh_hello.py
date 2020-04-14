90343c8ee7cf82ea0767bff36fd8f7f23170e6cd
jameshang <james.hang@hotmail.com>
2020-04-14 21:29:16 +0800

    Update

--- hello/hh_hello.py
+++ hello/hh_hello.py
@@ -1,27 +1,16 @@
-def plus(a, b, c):
-    return a * b * c
+def add(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10):
+    c = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10
+    print(c)
+    print(c)
+    return c
 
 
-def add(a, b):
-    res = 0
-    for i in range(a, b + 1):
-        res = res + i
-    return res
+def sub(a, b):
+    print(a - b)
+    return a - b
 
-
-def test1(p1,p2):
-    a = 1
-    a = 2
-    a = 3
-    b = 1
-    b = 2
-    a = b
-    b = 3
-    print(a)
-    print(b)
-    print(p1)
-    print(p2)
 
 if __name__ == '__main__':
-    test1(3,5)
-
+    c1 = add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    c2 = sub(89, 77)
+    print(c1 * c2)
